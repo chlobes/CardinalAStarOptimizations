78394cd1e8f9ba6fb4559725d9483fa14380ecdd
Chlobes <example@example.com>
2023-05-31 09:27:59 +1000

    give the wall puncher some chill

--- graph_connect.h
+++ graph_connect.h
@@ -4,9 +4,7 @@
 #include "rng.h"
 
 //search for a random point of desired type, return INT_MAX if we can't find one
-Coord find(Graph graph, int width, int height, Cell target, u64* rng) {
-    int start_x = gen_random(rng) % width;
-    int start_y = gen_random(rng) % height;
+Coord find(Graph graph, int width, int height, Cell target, int start_x, int start_y) {
     int x = start_x;
     int y = start_y;
 
@@ -41,36 +39,59 @@ void flood(Graph graph, int width, int height, int x, int y) {
     flood(graph, width, height, x, y - 1);
 }
 
-//checks if all empty nodes of the graph are connected and randomly adds connections until they are 
+//checks if all empty nodes of the graph are connected and randomly adds connections until they are
 void connect_graph(Graph graph, int width, int height, u64* rng) {
-    Coord from, empty;
-    empty = find(graph, width, height, 0, rng);
-    flood(graph, width, height, empty.x, empty.y);
-    empty = find(graph, width, height, 0, rng);
+    Coord prev;
+    Coord next = find(graph, width, height, 0, 0, 0);
 
-    while (empty.x != INT_MAX) {
-        from = find(graph, width, height, 2, rng);
-        int dx = from.x < empty.x ? 1 : -1;
-        int dy = from.y < empty.y ? 1 : -1;
-        while (from.x != empty.x && from.y != empty.y) { //take random orthogonal steps towards empty to create a pathway
-            graph[from.x + width * from.y] = 2;
-            if (gen_random(rng) % 2) {
-                from.x += dx;
+    while (1) {
+        flood(graph, width, height, next.x, next.y);
+        prev = next;
+        next = find(graph, width, height, 0, prev.x, prev.y);
+        if (next.x == INT_MAX) break;
+        
+        while (graph[prev.x + width * prev.y]) { //take random steps towards a known empty space until we hit an empty space, this will always terminate
+            graph[prev.x + width * prev.y] = 2; //each time we take a step, destroy the wall on this tile, creating a pathway towards the next empty tile
+            int step_type = gen_random(rng) % 8; //50% chance of taking a random step, 50% chance of taking a step towards the goal
+            switch (step_type) {
+                case 0: //right
+                    if (prev.x + 1 >= width) continue;
+                    prev.x += 1;
+                    break;
+                case 1: //down
+                    if (prev.y + 1 >= height) continue;
+                    prev.y += 1;
+                    break;
+                case 2: //left
+                    if (prev.x - 1 < 0) continue;
+                    prev.x -= 1;
+                    break;
+                case 3: //up
+                    if (prev.y - 1 < 0) continue;
+                    prev.y -= 1;
+                    break;
+                default: //towards goal
+                    if (step_type % 2) { //towards goal x
+                        if (prev.x == next.x) continue;
+                        if (prev.x < next.x) {
+                            prev.x += 1;
+                        }
+                        else {
+                            prev.x -= 1;
+                        }
+                    }
+                    else { //towards goal y
+                        if (prev.y == next.y) continue;
+                        if (prev.y < next.y) {
+                            prev.y += 1;
+                        }
+                        else {
+                            prev.y -= 1;
+                        }
+                    }
             }
-            else {
-                from.y += dy;
-            }
         }
-        while (from.x != empty.x) {
-            graph[from.x + width * from.y] = 2;
-            from.x += dx;
-        }
-        while (from.y != empty.y) {
-            graph[from.x + width * from.y] = 2;
-            from.y += dy;
-        }
-        flood(graph, width, height, empty.x, empty.y);
-        empty = find(graph, width, height, 0, rng);
+        next = prev; //set next to whatever empty tile we found first
     }
     //we're finished, now set all the twos back to zeroes
     for (int i = 0; i < width * height; i++) {
